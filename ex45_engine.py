0b44b4be93a9d6e1d8d0b86cfa0340638d3bc309
Tucker <h.rosebrock@gmail.com>
2016-03-23 16:43:07 -0400

    added engine test file

---
+++ ex45_engine.py
@@ -0,0 +1,53 @@
+from sys import exit
+from random import randint
+
+class Room(object):
+
+    def enter(self):
+	    print "This shouldn't show up ever, since this is just our base class."
+	    exit(1)
+
+    def replay(self):
+	    pass
+
+class Engine(object):
+
+    def __init__(self, room_map):
+        self.room_map = room_map
+
+    def play(self):
+        current_room = self.room_map.opening_room()
+        last_room = self.room_map.next_room('finished')
+
+        while current_room != last_room:
+            next_room_name = current_room.enter()
+            current_room = self.room_map.next_room(next_room_name)
+
+        current_room.enter()
+
+class Map(object):
+	# create a dictionary of the rooms
+	# then create a list of their keys
+	# pick a random room, pop it (so it's not in the array) and return it
+    rooms = {
+        'bedroom': Bedroom(),
+        'koi_pond': KoiPond(),
+        'library': Library(),
+        'basement': Basement(),
+        'ornate_room': OrnateRoom(),
+        'finished': Finished()
+    }
+
+    def __init__(self, start_room):
+        self.start_room = start_room
+		
+    def next_room(self, room_name):
+        val = Map.rooms.get(room_name)
+        return val
+		
+    def opening_room(self):
+        return self.next_room(self.start_room) 
+		
+a_map = Map('bedroom')
+a_game = Engine(a_map)
+a_game.play()
