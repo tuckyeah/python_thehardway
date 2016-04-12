7d8b25da55a60d2a72921be99547a37d9db64be2
Tucker <h.rosebrock@gmail.com>
2016-04-12 13:58:43 -0400

    updated app.py with GameEngine

--- projects/gothonweb/bin/app.py
+++ projects/gothonweb/bin/app.py
@@ -1,24 +1,50 @@
 import web
-
-web.config.debug = False
+from gothonweb import map
 
 urls = (
-    "/count", "count",
-    "/reset", "reset"
+    '/game', 'GameEngine',
+    '/', 'Index'
 )
-app = web.application(urls, locals())
-store = web.session.DiskStore('sessions')
-session = web.session.Session(app, store, initializer={'count': 0})
 
-class count:
+app = web.application(urls, globals())
+
+# little hack so that debug mode works with sessions
+if web.config.get('_session') is None:
+    store = web.session.DiskStore('sessions')
+    session = web.session.Session(app, store, initializer={'room':None})
+    web.config._session = session
+
+else:
+    session = web.config._session
+
+render = web.template.render('templates/', base="layout")
+
+
+class Index(object):
     def GET(self):
-        session.count += 1
-        return str(session.count)
+        #this is used to 'setup' the session with starting values
+        session.room = map.START
+        web.seeother("/game")
+
+
+class GameEngine(object):
+
+    def GET(self):
+        if session.room:
+            return render.show_room(room=session.room)
+        else:
+        #why is this here? do you need it?
+        return render.you_died()
 		
-class reset:
-    def GET(self):
-        session.kill()
-        return ""
+    def POST(self):
+        form = web.input(action=None)
+
+        #there is a bug here, can you fix it?
+        if session.room and form.action:
+            session.room = session.room.go(form.action)
+            return session.room
+
+        web.seeother("/game")
 		
 if __name__ == "__main__":
     app.run()
